f2d1a95d2c4f3171742c77aca85e4586d83a72b9
chrisnoyes23 <46818687+chrisnoyes23@users.noreply.github.com>
2019-01-23 15:05:38 -0500

    Hello World project

---
+++ HelloWorldChris.java
@@ -0,0 +1,9 @@
+public class HelloWorldChris {
+    public static void main(String[] args) {
+        System.out.println("This is my Hello World Project!");
+        System.out.println("I'm going to draw a Smiley Face!");
+        System.out.println(" | |");
+        System.out.println("I___I");
+
+    }
+}
