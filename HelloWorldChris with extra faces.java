ffe0b41af31d5d95c8a0669348b0eaa721182d0a
chrisnoyes23 <46818687+chrisnoyes23@users.noreply.github.com>
2019-01-28 09:44:10 -0500

    this has the String variable thing

---
+++ HelloWorldChris with extra faces.java
@@ -0,0 +1,18 @@
+public class HelloWorldChris {
+    public static void main(String[] args) {
+        String eyes = " I I";
+        String mouth = "I___I";
+        String frown = "/---/";
+        String bored = "_____";
+            System.out.println("This is my Hello World Project!");
+            System.out.println("I'm going to draw a Smiley Face!");
+            System.out.println(eyes);
+            System.out.println(mouth);
+            System.out.println("Now here is a Frowny Face!");
+            System.out.println(eyes);
+            System.out.println(frown);
+            System.out.println("Now here is a bored face!");
+            System.out.println(eyes);
+            System.out.println(bored);
+    }
+}
--- HelloWorldChris.java
+++ HelloWorldChris.java
@@ -1,9 +1,10 @@
 public class HelloWorldChris {
     public static void main(String[] args) {
-        System.out.println("This is my Hello World Project!");
-        System.out.println("I'm going to draw a Smiley Face!");
-        System.out.println(" | |");
-        System.out.println("I___I");
-
+        String eyes = I_I;
+        String mouth = I___I;
+            System.out.println("This is my Hello World Project!");
+            System.out.println("I'm going to draw a Smiley Face!");
+            System.out.println("eyes");
+            System.out.println("mouth");
     }
 }
