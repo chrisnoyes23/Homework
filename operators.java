8a0856d553bcab47b2bd750db3f52218fd800bfa
chrisnoyes23 <46818687+chrisnoyes23@users.noreply.github.com>
2019-02-04 09:43:57 -0500

    little calculator

---
+++ operators.java
@@ -0,0 +1,13 @@
+public class operators
+{
+        public static void main(String[] args)
+
+        {
+            int x = 5;
+            int y = 2;
+            System.out.printf("%d+%d=%d\n" , x , y , x+y );
+            System.out.printf("%d-%d=%d\n" , x , y , x-y );
+            System.out.printf("%dx%d=%d\n" , x , y , x*y );
+            System.out.printf("%d/%d=%d\n" , x , y , x/y );
+        }
+}
